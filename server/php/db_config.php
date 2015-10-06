dc18cfca546a1f1632b646f5cadbeec52727e91d
schleif <fiel@live.de>
2015-10-06 20:45:38 +0200

    rename database

--- server/php/db_config.php
+++ server/php/db_config.php
@@ -6,6 +6,6 @@
 	
 	define('DB_USER', "PotatoServer"); // db user
 	define('DB_PASSWORD', "potato"); // db password
-	define('DB_DATABASE', "potato"); // db name
+	define('DB_DATABASE', "potatoDB"); // db name
 	define('DB_SERVER', "localhost"); // db server
 ?>
