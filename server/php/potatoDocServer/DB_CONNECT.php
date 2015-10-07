66e05a69468b52cb4945f86562ba7bbf6dc9a2a8
schleif <fiel@live.de>
2015-10-07 11:46:33 +0200

    change database connection to mysqli connection

--- server/php/potatoDocServer/DB_CONNECT.php
+++ server/php/potatoDocServer/DB_CONNECT.php
@@ -13,6 +13,8 @@
 		* DB_SERVER - serveradress of the database
 	*/
 	class DB_CONNECT {
+            
+            private $db;
 		
 		// constructor
 		function __construct() {
@@ -34,14 +36,14 @@
 			require_once DB_CONFIG;
 			
 			// Connecting to mysql database
-			$db = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+			$this->db = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
                         
-                        if($db->connect_errno > 0) {
-                            die('Unable to connect to database [' . $db->connect_error . ']');
+                        if($this->db ->connect_errno > 0) {
+                            die('Unable to connect to database [' . $this->db->connect_error . ']');
                         }
 			
 			// returing connection cursor
-			return $db;
+			return $this->db;
 		}
 		
 		/**
@@ -49,7 +51,7 @@
 		*/
 		function close() {
 			// closing db connection
-			mysql_close();
+			$this->db->close();;
 		}
 		
 	}
