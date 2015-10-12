ce5d83172e61fefce5c35cde945a333f24471870
schleif <fiel@live.de>
2015-10-12 08:54:51 +0200

    minor changes
    procedure was wrong named

--- server/php/potatoDocServer/selectEigenschaft.php
+++ server/php/potatoDocServer/selectEigenschaft.php
@@ -6,4 +6,4 @@ define('DB_FUNCTIONS', __DIR__ . '/DB_FUNCTIONS.php');
 // import database connection variables
 require_once DB_FUNCTIONS;
 
-select("selectParzellen");
+select("selectEigenschaft");
